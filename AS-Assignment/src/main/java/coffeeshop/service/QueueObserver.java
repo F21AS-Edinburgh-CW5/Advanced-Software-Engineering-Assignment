30e6c0592c0b920aa567badada2e5020c90fddab
CodeExercise-pc <lifeshopingdining@163.com>
2026-03-29 19:03:33 +0100

    Update QueueObserver.java
    iteration2

--- AS-Assignment/src/main/java/coffeeshop/service/QueueObserver.java
+++ AS-Assignment/src/main/java/coffeeshop/service/QueueObserver.java
@@ -1,1 +1,11 @@
+package coffeeshop.service;
+
+import coffeeshop.model.SimulationSnapshot;
+
+/**QueueObserver Interface
+ * Observer used for queue changes .
+ */
+public interface QueueObserver {
+    void onQueueChanged(SimulationSnapshot snapshot);
+}
 
