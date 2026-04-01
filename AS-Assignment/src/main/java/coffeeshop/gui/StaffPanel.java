92b2a8fc1cf0334ea19c247dbffefa3ddf8c0252
Yize-D <daiyize24@gmail.com>
2026-04-01 13:35:11 +0800

    Update StaffPanel.java

--- AS-Assignment/src/main/java/coffeeshop/gui/StaffPanel.java
+++ AS-Assignment/src/main/java/coffeeshop/gui/StaffPanel.java
@@ -24,6 +24,7 @@ import java.text.DecimalFormat;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.ArrayList;
 
 /**
  * StaffPanel displays one column per staff member and updates itself from
